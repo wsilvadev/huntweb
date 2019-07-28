58bbd6694cb4ab75f0cb380d21eae6d9ebe0d208
Willian <wsilvadev@gmail.com>
2019-07-28 13:11:26 -0300

    listing product on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import Header from './components/Header';
-
+import './style.css'
+import Main from './components/pages/main/index'
 const App = () =>(
-    <div className="App">
+   
+   <div className="App">
      <Header/>
+     <Main/>
     </div>
 );
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import "./style.css";
 
-const Header = () => (<header id="main_header">Willian</header>);
+const Header = () => <header id="main_header">Willian</header>;
 
 
 export default Header;
