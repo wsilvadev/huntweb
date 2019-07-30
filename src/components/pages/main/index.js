2ec52ccbe874648e6e6946e503147b04fed36934
Willian <wsilvadev@gmail.com>
2019-07-29 22:14:10 -0300

    Giving a button for my page

--- src/components/pages/main/index.js
+++ src/components/pages/main/index.js
@@ -5,17 +5,47 @@ import "./style.css"
 export default class Main extends Component{
    state = {
        products:[],
+       productInfo : {},
+       page : 1,
    }
-   
+    
+   prevPage = () =>{
+  
+    const {page, productInfo} = this.state;
+  
+    if(page == 1) return;
+
+    const pageNumber = page -1;
+
+    this.loadProducts(pageNumber);
+   }
+
+   nextPage = () =>{
+    const {page, productInfo } = this.state;
+
+    if(page == productInfo.pages) return;
+    
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+   }
+
     componentDidMount(){   
-        this.lodadProducts();
+        this.loadProducts();
     }
-    lodadProducts = async ()=>{
-        const response = await api.get('/products');
-        this.setState({products: response.data.docs})
+
+    loadProducts = async ( page = 1)=>{
+
+        const response = await api.get(`/products?page=${page}`);
+
+        const {docs, ...productInfo} = response.data;
+
+        this.setState({products: docs, productInfo, page});
     }
+
     render(){
         const { products } = this.state;
+        
         return (
             <div className="product-list">
                  {products.map(product => (
@@ -24,8 +54,15 @@ export default class Main extends Component{
                     <strong>{product.title}</strong>
                     <p>{product.description}</p>
 
+                    <a href="">Acessar</a>
               </article>
+              
                 ))}
+
+                <div className="actions">
+                    <button onClick={this.prevPage}>Prev</button>
+                    <button  onClick={this.nextPage}>Next</button>
+                </div>
             </div>
         );
     }
